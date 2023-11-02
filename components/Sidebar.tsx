87e4aaeb82c88a4f5d61cf7eaf18822963c28509
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2023-11-02 14:03:49 +0200

    Initialised routing

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { usePathname} from 'next/navigation'
-
+import { useMemo } from 'react'
+import {HiHome} from 'react-icons/hi'
+import { BiSearch} from 'react-icons/bi'
 interface SidebarProps {
     children: React.ReactNode;
 }
@@ -8,9 +10,31 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({
     children
 }) => {
+
+    const pathname = usePathname();
+
+    const routes = useMemo(() => [
+        {
+            icon: HiHome,
+            label: 'Home',
+            active: pathname !== '/search',
+              href: '/',
+        },
+        {
+            icon: BiSearch,
+            label: 'Search',
+            active: pathname === '/search',
+            href: '/search',
+        }
+    ], [pathname]);
+
     return (
-        <div>
-            {children}
+        <div className = 'flex h-full'>
+            <div
+            className='
+            hidden
+            md:flex'
+            ></div>    
         </div>
     )
 }
