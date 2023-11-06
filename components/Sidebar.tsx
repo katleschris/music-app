57d40cd98adaeee1a5891a696ae39a6cef95d2c8
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2023-11-06 02:51:04 +0200

    Completed routing and menu icons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,6 +3,8 @@ import { usePathname} from 'next/navigation'
 import { useMemo } from 'react'
 import {HiHome} from 'react-icons/hi'
 import { BiSearch} from 'react-icons/bi'
+import Box from './Box'
+import SidebarItem from './SidebarItem'
 interface SidebarProps {
     children: React.ReactNode;
 }
@@ -33,8 +35,34 @@ const Sidebar: React.FC<SidebarProps> = ({
             <div
             className='
             hidden
-            md:flex'
-            ></div>    
+            md:flex
+            flex-col
+            gap-y-2
+            bg-black
+            h-full
+            w-[300px]
+            p-2'
+            >
+                <Box>
+                    <div className='
+                    flex
+                    flex-col
+                    gap-y-4
+                    px-5
+                    py-4
+                    '
+                    >
+                        {routes.map((item) => (
+                            <SidebarItem
+                            key={item.label}
+                            {...item}/>
+                        ))}
+                    </div>
+                </Box>
+                <Box className='overflow-y-auto hfull'>
+                    Song Library
+                </Box>
+            </div>    
         </div>
     )
 }
