f09742b0de9672a8000c3cb4840c44f7aedf42bf
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2023-11-07 01:33:22 +0200

    Added buttons to move forward and back

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,6 @@
 import Header from '../../components/Header'
+
+
 export default function Home() {
   return (
     <div className='
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,9 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import { twMerge } from 'tailwind-merge';
+import { fileURLToPath } from 'url';
+import { RxCaretLeft, RxCaretRight } from 'react-icons/rx'
+
 interface HeaderProps {
     children: React.ReactNode;
     className?: string;
@@ -7,8 +13,58 @@ const Header: React.FC<HeaderProps> = ({
     children,
     className,
 }) => {
+    const router = useRouter();
+
+    const handleLogout = () =>{
+        //Logout the user
+    }
+
     return (
-        <div>I AM A Header</div>
+        <div className={twMerge(`
+        h-fit
+        bg-gradient-to-b
+        from-purple-800
+        p-6
+        `, className)}>
+            <div className='
+            w-full
+            mb-4
+            flex
+            items-center
+            justify-between'>
+                <div className='
+                    hidden
+                    md:flex
+                    gap-x-2
+                    items-center
+                    '>  
+                    <button 
+                    onClick={() => router.back()}
+                    className='
+                      rounded-full
+                      bg-black
+                      flex
+                      items-center
+                      justify-center
+                      hover:opacity-75
+                      transition'   >
+                        <RxCaretLeft size={35} className='text-white'/>
+                    </button>
+                    <button 
+                    onClick={() => router.forward()}
+                    className='
+                      rounded-full
+                      bg-black
+                      flex
+                      items-center
+                      justify-center
+                      hover:opacity-75
+                      transition'   >
+                        <RxCaretRight size={35} className='text-white'/>
+                    </button>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Header
