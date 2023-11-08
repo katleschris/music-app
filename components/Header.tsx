ebd26ec0a282e2af7b69fa7f10d4dcfcbc384b6b
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2023-11-08 02:35:42 +0200

    Added and styled log in button

--- components/Header.tsx
+++ components/Header.tsx
@@ -99,8 +99,23 @@ const Header: React.FC<HeaderProps> = ({
                 gap-x-4'>
                     <>
                         <div>
-                            <Button>
+                            <Button
+                            onClick={() => {}}
+                            className=' 
+                            bg-transparent
+                            text-neutral-300
+                            font-medium'>
                                 Sign up
+                            </Button>
+                        </div>
+                        <div>
+                            <Button
+                            onClick={() => {}}
+                            className=' 
+                            bg-white
+                            px-6
+                            py-2'>
+                                Log in
                             </Button>
                         </div>
                     </>
