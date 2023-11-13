55ef9f9ca09eaf2611deea77d1cd33ccc90b2505
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2023-11-13 15:39:23 +0200

    Completed Input component

--- components/UploadModel.tsx
+++ components/UploadModel.tsx
@@ -1,25 +1,53 @@
 'use client'
 
+import { useForm, FieldValues, SubmitHandler } from 'react-hook-form';
+import {useState} from 'react'
+
 import Model from './Model'
 import useUploadModel from '@/hooks/useUploadModel'
+import Input from './Input';
 
 const UploadModel = () => {
+    const [isLoading, setIsLoading] = useState()
     const uploadModel = useUploadModel();
+
+    const {register, handleSubmit, reset} = useForm<FieldValues>({
+        defaultValues: {
+            author: '',
+            title:'',
+            song: null,
+            image: null,
+        }
+    })
 
     const onChange = (open: boolean) => {
         if(!open){
-            //reset form
+            reset();
             uploadModel.onClose();
         }
     }
 
+    const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+        //upload to supabase
+    }
+
     return (
         <Model
-            title='Upload model title'
-            description='Upload model description'
+            title='Add a song'
+            description='Upload an mp3 file'
             isOpen = {uploadModel.isOpen}
             onChange={onChange}
-        >Upload Content
+        >
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <Input
+                    id= 'title'
+                    disabled = {isLoading}
+                    {...register('title', { required: true})}
+                    placeholder='Song title'
+                />
+            </form>
         </Model>
     )
 }
