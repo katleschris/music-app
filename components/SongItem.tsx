3c9bd8f34bdab304e40d55f655e2769efeb7681d
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2023-11-27 15:54:18 +0200

    Displayed song data

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -1,6 +1,8 @@
 'use client'
-
+import useLoadImage from '@/hooks/useLoadImage'
 import {Song} from '@/types'
+
+import Image from 'next/image'
 
 interface SongItemProps {
     data: Song;
@@ -11,8 +13,50 @@ const SongItem: React.FC<SongItemProps> = ({
     data,
     onClick
 }) => {
+    const imagePath = useLoadImage(data);
     return(
-        <div>Song Item</div>
+        <div
+            onClick = {() => onClick(data.id)}
+            className='
+                relative
+                group
+                flex
+                flex-col
+                items-center
+                justify-center
+                rounded-md
+                overflow-hidden
+                gap-x-4
+                bg-neutral-499/5
+                cursor-pointer
+                hover:bg-neutral-400/10
+                transition 
+                p-3
+            '>
+                <div
+                    className='
+                        relative
+                        aspect-square
+                        w-full
+                        h-full
+                        rounded-md
+                        overflow-hidden'>
+                            <Image
+                                className ='object-cover'
+                                src={imagePath || '/images/liked.png'}
+                                fill
+                                alt='Image'
+                            />
+                        </div>
+                        <div className='flex flex-col items-start w-full p-4 gap-y-1'>
+                            <p className='font-semibold truncate w-full'>
+                                {data.title}
+                            </p>
+                            <p>
+                                By {data.author}
+                            </p>
+                        </div>
+            </div>
     );
 }
 
