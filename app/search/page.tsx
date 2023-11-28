d1d23bbac0a57aa13f75b739e5ecaa412870459b
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2023-11-28 20:07:55 +0200

    Completed search by title

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,7 @@
 import getSongsByTitle from "@/actions/getSongsByTitle";
 import Header from '@/components/Header'
 import SearchInput from '@/components/SearchInput'
+import SearchContent from './components/SearchContent'
 
 interface SearchProps {
     searchParams: {
@@ -28,7 +29,9 @@ const Search = async ({ searchParams }: SearchProps) => {
                     <SearchInput />
                 </div>
             </Header>
-            
+            <SearchContent songs={songs} />
         </div>
     )
 }
+
+export default Search
