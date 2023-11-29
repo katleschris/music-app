c3ffd439e2f4ec47a30d288da18da31d2c05e181
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2023-11-29 14:36:22 +0200

    Rendered Liked songs playlist

--- app/liked/page.tsx
+++ app/liked/page.tsx
@@ -1,5 +1,6 @@
 import getLikedSongs from '@/actions/getLikedSongs'
 import Header from '@/components/Header'
+import LikedContent from './components/LikedContent'
 
 import Image from 'next/image'
 
@@ -63,6 +64,7 @@ const Liked = async () => {
                         </div>
                     </div>
                 </Header>
+                <LikedContent songs={songs} />
             </div>
     );
 }
