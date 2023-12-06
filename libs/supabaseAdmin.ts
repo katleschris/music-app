4aef3f671c06eec939326f754ecc5596a544c662
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2023-12-06 13:05:31 +0200

    "Added function for handling Stripe product data

--- libs/supabaseAdmin.ts
+++ libs/supabaseAdmin.ts
@@ -12,3 +12,23 @@ export const supabaseAdmin = createClient<Database>(
     process.env.SUPABASE_SERVICE_ROLE_KEY || ''
 );
 
+const upsertProductRecord = async (product: Stripe.Product) =>{
+    const productData: Product = {
+        id: product.id,
+        active: product.active,
+        name: product.name,
+        description: product.description ?? undefined,
+        image: product.images?.[0] ?? null,
+        metadata: product.metadata
+    };
+
+    const { error } = await supabaseAdmin
+        .from('products')
+        .upsert([productData]);
+    
+    if (error) {
+            throw error;
+    }
+
+    console.log(`Product inserted/uupdated: ${product.id}`);
+}
